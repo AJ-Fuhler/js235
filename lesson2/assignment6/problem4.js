73c8755376946de21984d2cdc8d67bfd77cc5b4c
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-02 17:04:21 -0500

    added assignment6 problem4

---
+++ lesson2/assignment6/problem4.js
@@ -0,0 +1,11 @@
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject({ error: "User not found" }), 500);
+  });
+}
+
+fetchUserData()
+  .catch(({error}) => {
+    console.log(error);
+  })
+  .finally(() => console.log('Fetching complete'));
