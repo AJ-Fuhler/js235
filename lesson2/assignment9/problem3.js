376bb89d7ce26e8f4362c73420255d9b0495b5ce
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-02 17:05:24 -0500

    added assignment9 problem3

---
+++ lesson2/assignment9/problem3.js
@@ -0,0 +1,18 @@
+function flakyService() {
+  return new Promise((resolve, reject) => {
+    if (Math.random() > 0.5) {
+      resolve("Operation successful");
+    } else {
+      reject("Operation failed");
+    }
+  });
+}
+
+const services = [flakyService(), flakyService(), flakyService()];
+
+Promise.allSettled(services)
+  .then(services => {
+    services.forEach(service => {
+      console.log(service);
+    });
+  });
