a0df54aea84b2ac82a68193d40f1a04965cd3fed
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-03 15:00:45 -0500

    added assignment11 problem1

---
+++ lesson2/assignment11/problem1.js
@@ -0,0 +1,21 @@
+function downloadFilePromise() {
+  return new Promise((resolve) => {
+    console.log("Downloading file...");
+    setTimeout(() => {
+      resolve("Download complete!");
+    }, 1500);
+  });
+}
+
+async function asyncDownloadFile() {
+  console.log("Downloading file...");
+  let message = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Download complete!");
+    }, 1500);
+  });
+
+  console.log(message);
+}
+
+asyncDownloadFile();
