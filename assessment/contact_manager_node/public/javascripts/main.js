fc66ddb28188dd10267a602c93e255309f0de50f
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-25 11:45:04 -0500

    added all remaining features

--- assessment/contact_manager_node/public/javascripts/main.js
+++ assessment/contact_manager_node/public/javascripts/main.js
@@ -9,8 +9,9 @@ class ContactManager {
     this.emptySearchPlaceholder = document.querySelector('.empty-search-contacts-placeholder');
     this.formPlaceholder = document.querySelector('.form-placeholder');
     this.contactButtons = document.querySelectorAll('.btn.contact-btn');
-    this.tagButton = document.querySelector('.btn.tag-btn');
+    this.addTagButton = document.querySelector('.btn.tag-btn');
     this.searchQuery = document.getElementById('search-query');
+    this.tagsUL = document.querySelector('.tags');
     this.contacts = [];
     this.tags = [];
 
@@ -24,28 +25,108 @@ class ContactManager {
   bindEvents() {
     this.input.addEventListener('input', this.valueChanged);
     this.contactButtons.forEach(btn => btn.addEventListener('click', this.handleAddContactButton.bind(this)));
-    // this.tagButton.addEventListener('click', this.handleTagButton.bind(this));
+    this.addTagButton.addEventListener('click', this.handleAddTagButton.bind(this));
     document.addEventListener('reset', this.handleCancelButton.bind(this));
     document.addEventListener('submit', this.handleSubmit.bind(this));
     document.addEventListener('click', this.handleClick.bind(this));
   }
 
+  handleAddTagButton(event) {
+    event.preventDefault();
+
+    this.rowContainer.classList.add('hide');
+    this.contactList.classList.add('hide');
+    this.emptyContactsPlaceholder.classList.add('hide');
+    this.emptySearchPlaceholder.classList.add('hide');
+
+    this.renderTagForm();
+  }
+
+  renderTagForm() {
+    let html = `<form action="/tags" method="post">
+  <h3>Add a Tag</h3>
+  <label>
+    Tag:
+    <input type="text" name="tag" id="tag">
+  </label>
+  <button type="submit">Submit</button>
+  <button type="reset">Cancel</button>
+</form>`;
+
+    this.formPlaceholder.innerHTML = html;
+  }
+
   handleClick(event) {
     if (event.target.classList.contains('edit')) {
       this.handleEditContactButton(event);
+    } else if (event.target.classList.contains('delete')) {
+      this.handleDeleteContactButton(event);
+    } else if (event.target.classList.contains('tag')) {
+      this.handleTagSearchButton(event);
+    }
+  }
+
+  handleTagSearchButton(event) {
+    event.preventDefault();
+
+    this.contactList.innerHTML = '';
+    let tagName = event.target.textContent;
+
+    if (tagName === 'All') {
+      this.renderContacts(this.contacts);
+      return;
+    }
+
+    let matches = this.contacts.filter(contact => {
+      let contactTags = contact.tags.split(',');
+      return contactTags.some(contactTag => contactTag.toLowerCase() === tagName.toLowerCase());
+    });
+
+    this.renderContacts(matches);
+  }
+
+  async handleDeleteContactButton(event) {
+    event.preventDefault();
+
+    const userConfirmed = confirm("Are you sure you want to delete this contact?");
+
+    if (userConfirmed) {
+      let id = event.target.getAttribute('href').match(/\d+/g)[0];
+      let contact = this.contacts.find(contact => contact.id === id);
+      let json = JSON.stringify(contact);
+
+      await fetch(event.target.getAttribute('href'), {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json,
+      });
+
+      this.reset();
     }
   }
 
   handleCancelButton(event) {
     this.formPlaceholder.innerHTML = '';
-    this.rowContainer.classList.remove('hide');
-    this.contactList.classList.remove('hide');
+    this.reset();
   }
 
   async handleSubmit(event) {
     event.preventDefault();
-
     let form = event.target;
+
+    if (form.action.endsWith('/tags')) {
+      let tag = new FormData(form).get('tag');
+      if (!this.tags.includes(tag)) {
+        this.tags.push(tag);
+      }
+      this.reset();
+      return;
+    }
+
+
+
     let method = form.dataset.method? form.dataset.method : form.method;
     let formData = new FormData(form);
     let tags = formData.getAll('tags').join(',');
@@ -82,26 +163,35 @@ class ContactManager {
     this.contactList.classList.remove('hide');
     this.rowContainer.classList.remove('hide');
     this.formPlaceholder.innerHTML = '';
-    this.renderContacts()
-    this.tags = this.getTags();
-
+    this.renderContacts(this.contacts);
+    this.tags = this.getTags(this.tags);
+    this.renderTags();
   }
 
-  getTags() {
+  getTags(existingTags) {
     let allTags = this.contacts
       .flatMap(contact => {
         if (!contact.tags) return null;
         return contact.tags.split(',');
       })
+      .concat(existingTags)
       .filter(tag => tag !== null);
 
+    allTags.unshift('All');
     let uniqueTags = [...new Set(allTags)];
-
     return uniqueTags;
   }
 
-  renderContacts() {
-    this.contacts.forEach(contact => {
+  renderTags() {
+    this.tagsUL.innerHTML = '';
+    this.tags.forEach(tag => {
+      let html = `<li><a href="${tag}" class="tag">${tag}</a></li>`;
+      this.tagsUL.insertAdjacentHTML('beforeend', html);
+    });
+  }
+
+  renderContacts(contacts) {
+    contacts.forEach(contact => {
       let html = `<li><h3 class="contact-name">${contact.full_name}</h3>
 <dl>
   <dt>Phone Number:</dt>
@@ -112,8 +202,8 @@ class ContactManager {
   <dd>${contact.tags || 'none'}</dt>
 </dl>
 <div class="contact-btn-wrapper">
-  <a href="/contacts/${contact.id}" class="edit">Edit</a>
-  <a href="/contacts/${contact.id}" class="delete">Delete</a>
+  <a href="/api/contacts/${contact.id}" class="edit">Edit</a>
+  <a href="/api/contacts/${contact.id}" class="delete">Delete</a>
 </div></li>`
 
     this.contactList.insertAdjacentHTML('beforeend', html);
@@ -121,7 +211,25 @@ class ContactManager {
   }
 
   async valueChanged() {
+    this.emptySearchPlaceholder.classList.add('hide');
+
+    this.contactList.innerHTML = '';
     let value = this.input.value;
+    if (value.length === 0) {
+      this.renderContacts(this.contacts);
+      return;
+    }
+
+    let matches = this.contacts.filter(contact => {
+      return contact.full_name.toLowerCase().startsWith(value.toLowerCase());
+    });
+
+    if (matches.length > 0) {
+      this.renderContacts(matches);
+    } else {
+      this.searchQuery.textContent = value;
+      this.emptySearchPlaceholder.classList.remove('hide');
+    }
   }
 
   renderContactForm(method, contact) {
@@ -132,8 +240,10 @@ class ContactManager {
     }
 
     let tagHTML = this.tags.map(tag => {
-      let checked = tags.includes(tag) ? 'checked' : '';
-      return `<div><input type="checkbox" id="${tag}" name="tags" value="${tag}" ${checked}><label for="${tag}">  ${tag}</label></div>`
+      if (tag !== 'All') {
+        let checked = tags.includes(tag) ? 'checked' : '';
+        return `<div><input type="checkbox" id="${tag}" name="tags" value="${tag}" ${checked}><label for="${tag}">  ${tag}</label></div>`
+      }
     }).join('');
 
     if (method === 'post') {
@@ -199,6 +309,8 @@ class ContactManager {
 
     this.rowContainer.classList.add('hide');
     this.contactList.classList.add('hide');
+    this.emptyContactsPlaceholder.classList.add('hide');
+    this.emptySearchPlaceholder.classList.add('hide');
     this.renderContactForm('post');
   }
 
